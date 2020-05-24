e35a62286aa554116e968fde3be94ca31c1e2d2c
jethro-magaji <jemagaji38@gmail.com>
2020-05-24 19:34:15 +0100

    first commit

---
+++ src/client/js/formHandler.js
@@ -0,0 +1,33 @@
+const fetchData = (text) => (fetch('http://localhost:8081/test', {
+    method: "POST",
+    credentials: "same-origin",
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ text })
+}).then(res => res.json()));
+
+
+function handleSubmit(event) {
+    event.preventDefault()
+
+    // check what text was put into the form field
+    let formText = document.getElementById('name').value
+    Client.checkForName(formText)
+    console.log("::: Form Submitted :::")
+
+    fetchData(formText)
+        .then(function(res) {
+            console.log(res)
+                // document.getElementById('results').innerHTML = `Polarity: ${res.polarity}<br>Subectivity: ${res.subjectivity}<br>Text: ${res.text}`;
+            document.getElementById('polarity').innerHTML = `Polarity: ${res.polarity}`;
+            document.getElementById('subjectivity').innerHTML = `Subectivity: ${res.subjectivity}`;
+            document.getElementById('text-data').innerHTML = `Text: ${res.text}`;
+
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        })
+}
+
+export { handleSubmit, fetchData }
